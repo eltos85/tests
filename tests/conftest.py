e127cdf5623a4c163400a4c9984a06ed65bcde93
eltos85 <38495910+eltos85@users.noreply.github.com>
2022-06-28 15:41:40 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,25 +5,28 @@ from src.bet_boom_prod import SearchHelper
 from src.games_page import GamesPagesSearchHelper
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
     chrm_caps = webdriver.DesiredCapabilities.CHROME.copy()
     chrm_caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-    driver = webdriver.Chrome(options=options, service_args=["--verbose"], desired_capabilities=chrm_caps)
-    driver.implicitly_wait(3)
+    # driver = webdriver.Chrome(options=options, desired_capabilities=chrm_caps)
+    driver = webdriver.Remote("http://localhost:4444/wd/hub", options=options,
+                              desired_capabilities=chrm_caps)
+    driver.implicitly_wait(10)
     driver.set_window_size(1600, 1000)
     yield driver
     driver.quit()
 
 
-@pytest.fixture(autouse=True, scope="function")
+@pytest.fixture()
 def page(browser):
     yield SearchHelper(browser)
 
 
-@pytest.fixture(autouse=True, scope="function")
+@pytest.fixture()
 def game(browser):
     yield GamesPagesSearchHelper(browser)
 
